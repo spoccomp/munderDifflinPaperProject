41b6108ee7095a913a87d2e93f0369b3d30859d1
richard trapanese <richardtrapanese@gmail.com>
2018-08-07 23:04:23 -0400

    updates all js, tests good

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-// function playPaperCrinkle() {
-//     document.getElementById('audiotag').play();
-// }
-// //document.getElementById("audiotag").onload = function() {playPaperCrinkle()};
-// function playVid() { 
-//     document.getElementById("myVideo").play(); 
-// } 
 
-//let checkIt = document.querySelectorAll("copyImg");
-//checkIt.addEventListener("mouseover",function(event){funnyFunction()});
-function funnyFunction(event){ 
-    event.style.height = "400px";
-    event.style.width = "400px";
+
+//below enlarges and normalizes the funny image
+let growShrink = document.querySelector(".copyImg");
+growShrink.addEventListener("mouseover",funnyFunction);
+function funnyFunction(){ 
+    event.target.style.height = "400px";
+    event.target.style.width = "400px";
 }
-function backToNormal(event){
-    event.style.height = "200px";
-    event.style.width = "200px";
+growShrink.addEventListener("mouseout",backToNormal);
+function backToNormal(){
+    event.target.style.height = "200px";
+    event.target.style.width = "200px";
 }
-//document.querySelector("pFun").addEventListener("mouseover",makeItFunny);
-//document.querySelector("pFun").addEventListener("mouseout",makeItNormal);
-//let funny = document.querySelector("pFun");
-function makeItFunny(event){
-    event.style.color = "red";
-    //event.funny.style.color = "red";
-    //event.funny.style.fontWeight ="bold";
-    //document.querySelector("pFun").style.color = "red";
+
+//below changes the color of the text 
+let changeColor = document.querySelector(".pFun");
+changeColor.addEventListener("mouseover",makeItFunny);
+function makeItFunny(){
+    event.target.style.color = "red";
 }
-function makeItNormal(event){
-    event.style.color = "black";
+changeColor.addEventListener("mouseout",makeItNormal);
+function makeItNormal(){
+    event.target.style.color = "black";
 }
+
+//below two buttons show and hide team photo
+let btnShowTeam = document.querySelector(".btnSee");
+btnShowTeam.addEventListener("click", seeTeam);
 function seeTeam(){
-    //document.getElementById("hidden").style.display = 'block';
     document.getElementById("hidden").style.visibility = "visible";
     document.getElementById("hidden").style.marginLeft = "15%";
     document.getElementById("hidden").style.height = "400px";
@@ -40,34 +38,58 @@ function seeTeam(){
     document.getElementById("hidden").style.borderTopLeftRadius = "5px";
     document.getElementById("hidden").style.webkitBorderTopRightRadius = "5px";
 }
+let btnHideTeam = document.querySelector(".btnHide");
+btnHideTeam.addEventListener("click",hiddenTeam);
 function hiddenTeam(){
     document.getElementById("hidden").style.visibility = "hidden";
 }
+
+//below opens a new tab to watch competitor's commercial
+let btnComm = document.querySelector(".buttonStuff");
+btnComm.addEventListener("click", watchUTube);
 function watchUTube(){
     //opens a new tab and plays video
     window.open("https://youtu.be/aawxhGuYr2M");
 }
-function funnyImage(event){//this is blown up
-    event.style.height = "150px";
-    event.style.width = "150px";
-    //textBlowUp();    
-}
-function normImage(event){//this is normal
-    event.style.height = "50px";
-    event.style.width = "50px";
-    //textNormal();
-}
-//setInterval(() => h1Sales.hidden = !h1Sales.hidden, 1000 );
-// function textBlowUp(){
-//     document.getElementById("pBoss").visibility = "visible";
-//     document.getElementById("pBoss").style.fontSize = "10px";
-//     document.getElementById("pBoss").style.color = "black";
-//     document.getElementById("pBoss").style.fontFamily = "'Courier New', Courier, monospace";
 
+//below blows up manager images and normalizes them
+let topFunImg1 = document.getElementById("imgManger1"); 
+let topFunImg2 = document.getElementById("imgManger2");
+let topFunImg3 = document.getElementById("imgManger3");
+let topFunImg4 = document.getElementById("imgManger4");
+
+topFunImg1.addEventListener("mouseover",funnyImage);
+topFunImg2.addEventListener("mouseover",funnyImage);
+topFunImg3.addEventListener("mouseover",funnyImage);
+topFunImg4.addEventListener("mouseover",funnyImage);
+
+function funnyImage(){//this blows up manager image
+    event.target.style.height = "150px";
+    event.target.style.width = "150px";     
+}
+
+topFunImg1.addEventListener("mouseout",normImage);
+topFunImg2.addEventListener("mouseout",normImage);
+topFunImg3.addEventListener("mouseout",normImage);
+topFunImg4.addEventListener("mouseout",normImage);
+
+function normImage(){//this normalizes manager image
+    event.target.style.height = "50px";
+    event.target.style.width = "50px";
+}
+
+//////*****TEST CODE THAT WORKS*****///////
+// let sect1 = document.getElementById("section-1");
+// sect1.addEventListener("mouseover", makeRed);
+// function makeRed() {
+//     sect1.style.color = "red";
 // }
-// function textNormal(){
-//     document.getElementById("pBoss").style.visibility = "hidden";
+// sect1.addEventListener("mouseout", makeNormal);
+// function makeNormal() {
+//     sect1.style.color = "white";
 // }
+
+
 
 
  
